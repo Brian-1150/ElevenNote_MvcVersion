299fec206c7a404369db5fe07ce8baff63d9cf64
bpcam <bpcampassi@gmail.com>
2021-03-25 20:34:53 -0400

    NoteCreate and Changes to navBar

--- ElevenNoteWebMVC/Controllers/NoteController.cs
+++ ElevenNoteWebMVC/Controllers/NoteController.cs
@@ -5,16 +5,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ElevenNoteWebMVC.Controllers
-{
+namespace ElevenNoteWebMVC.Controllers {
     [Authorize]
-    public class NoteController : Controller
-    {
-        // GET: Note
-        public ActionResult Index()
-        {
+    public class NoteController : Controller {
+
+        //[C]RUD
+        //GET:  CreateNote View
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model) {
+            if (ModelState.IsValid) {
+
+            }
+            return View(model);
+        }
+
+        //C[R]UD
+        // GET: Index - List of notes
+        public ActionResult Index() {
             var model = new NoteListItem[0];
             return View(model);
         }
     }
+
+
 }
