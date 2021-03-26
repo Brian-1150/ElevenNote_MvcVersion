9b61261d6f3adeac8a00f597798592262523bac7
bpcam <bpcampassi@gmail.com>
2021-03-25 23:27:25 -0400

    Added success and fail messages to noteCreate

--- ElevenNoteWebMVC/Controllers/NoteController.cs
+++ ElevenNoteWebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,11 @@ using System.Web.Mvc;
 namespace ElevenNoteWebMVC.Controllers {
     [Authorize]
     public class NoteController : Controller {
+        private NoteService CreateNoteService() {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
 
         //[C]RUD
         //GET:  CreateNote View
@@ -18,16 +25,27 @@ namespace ElevenNoteWebMVC.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model) {
-            if (ModelState.IsValid) {
+            if (!ModelState.IsValid)
+                return View(model);
 
+            var service = CreateNoteService();
+            if (service.CreateNote(model)) {
+                TempData["SaveResult"] = "Your note was successfully created!";
+                return RedirectToAction("Index");
             }
+            ModelState.AddModelError("", "Note could not be created.");
+
             return View(model);
         }
+
+
 
         //C[R]UD
         // GET: Index - List of notes
         public ActionResult Index() {
-            var model = new NoteListItem[0];
+            var service = CreateNoteService();
+            //var model = new NoteListItem[0];
+            var model = service.GetNotes();
             return View(model);
         }
     }
