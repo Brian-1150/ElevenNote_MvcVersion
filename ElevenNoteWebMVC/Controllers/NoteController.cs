66211de1b50f8ecd56c03f2009277b108450ba95
bpcam <bpcampassi@gmail.com>
2021-03-30 19:51:37 -0400

    Add Edit and Delete Methods and views

--- ElevenNoteWebMVC/Controllers/NoteController.cs
+++ ElevenNoteWebMVC/Controllers/NoteController.cs
@@ -7,10 +7,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ElevenNoteWebMVC.Controllers {
+namespace ElevenNoteWebMVC.Controllers
+{
     [Authorize]
-    public class NoteController : Controller {
-        private NoteService CreateNoteService() {
+    public class NoteController : Controller
+    {
+        private NoteService CreateNoteService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
             return service;
@@ -18,18 +21,21 @@ namespace ElevenNoteWebMVC.Controllers {
 
         //[C]RUD
         //GET:  CreateNote View
-        public ActionResult Create() {
+        public ActionResult Create()
+        {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(NoteCreate model) {
+        public ActionResult Create(NoteCreate model)
+        {
             if (!ModelState.IsValid)
                 return View(model);
 
             var service = CreateNoteService();
-            if (service.CreateNote(model)) {
+            if (service.CreateNote(model))
+            {
                 TempData["SaveResult"] = " Your note was successfully created!";
                 return RedirectToAction("Index");
             }
@@ -42,7 +48,8 @@ namespace ElevenNoteWebMVC.Controllers {
 
         //C[R]UD
         // GET: Index - List of notes
-        public ActionResult Index() {
+        public ActionResult Index()
+        {
             var service = CreateNoteService();
             //var model = new NoteListItem[0];
             var model = service.GetNotes();
@@ -50,13 +57,71 @@ namespace ElevenNoteWebMVC.Controllers {
         }
 
         //GET:  Note Details
-        public ActionResult Details(int id) {
+        public ActionResult Details(int id)
+        {
             var svc = CreateNoteService();
             var model = svc.GetNoteById(id);
 
             return View(model);
         }
+
+        //CR[U]D
+        //Edit Note
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model = new NoteEdit
+            {
+                NoteId = detail.NoteId,
+                Title = detail.Title,
+                Content = detail.Content
+
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if (model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+
+            }
+            var service = CreateNoteService();
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated successfully!";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Your note could not be updated");
+            return View(model);
+        }
+
+        //CRU[D]
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateNoteService();
+            service.DeleteNote(id);
+            TempData["SaveResult"] = "Your note was deleted";
+
+            return RedirectToAction("Index");
+        }
+
+
     }
-
-
 }
